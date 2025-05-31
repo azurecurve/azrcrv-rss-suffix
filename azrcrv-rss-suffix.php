8b8c31d44f3e69e3d6482160aa1dd6f4c5ed26ad
azurecurve <github@azurecurve.co.uk>
2025-05-31 11:20:05 +0100

    Release v1.2.6

--- azrcrv-rss-suffix.php
+++ azrcrv-rss-suffix.php
@@ -3,8 +3,9 @@
  * ------------------------------------------------------------------------------
  * Plugin Name:		RSS Suffix
  * Description:		Provides opposite rss feed to that configured in ClassicPress
- * Version:			1.2.5
+ * Version:			1.2.6
  * Requires CP:		1.0
+ * Requires PHP:	7.4
  * Author:			azurecurve
  * Author URI:		https://development.azurecurve.co.uk/classicpress-plugins/
  * Plugin URI:		https://development.azurecurve.co.uk/classicpress-plugins/rss-suffix/
